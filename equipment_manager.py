072c715b800ba6ed05e747e409e62132d377d397
zzeuz <145396948+zzeuz@users.noreply.github.com>
2026-06-26 17:49:59 -0500

    Add files via upload

--- equipment_manager.py
+++ equipment_manager.py
@@ -10,7 +10,7 @@ import pyqtgraph as pg
 from PyQt5 import QtWidgets, QtCore, uic
 from PyQt5.QtWidgets import (QApplication, QMainWindow,
                              QDialog, QTableWidget, QTableWidgetItem,
-                             QLabel)
+                             QLabel, QPushButton)
 from PyQt5.QtCore import QTimer, QSize
 
 
@@ -42,44 +42,52 @@ class EquipmentManager(QMainWindow):
     acquisition class called 'DAQThread', simulating experimental
     data monitoring in real time.
     '''
+    connect_equipment = QtCore.pyqtSignal(object)
     def __init__(self):
         QMainWindow.__init__(self)
         uic.loadUi("equipment_manager.ui", self)
 
-        equipment_list = [("Lockin", "USB0::0xB506::0x2000::003051::INSTR","Connected"),
-                          ("Oscilloscope", "USB::EQUIPMENT::TAG2","Disconnected")]
+        self.lockin_button = QPushButton("Connect")
+        self.oscilloscope_button = QPushButton("Connect")
+        self.equipment_list = [("Lockin", "USB0::0xB506::0x2000::003051::INSTR", "Disconnected", self.lockin_button),
+                          ("Oscilloscope", "USB::EQUIPMENT::TAG2", "Disconnected", self.oscilloscope_button)]
 
         self.lockin = None
         self.oscilloscope = None
 
         self.mutex = QtCore.QMutex()
 
-        #self.listWidget.addItems(["first", "second"])
-
         self.rm = pyvisa.ResourceManager()
-        #print(self.rm.list_resources())
+
         self.listWidget.addItems(self.rm.list_resources())
-        #self.listWidget.clear()
         
         table = self.tableWidget
-        table.setRowCount(len(equipment_list))
-        table.setColumnCount(len(equipment_list[0]))
-        table.setHorizontalHeaderLabels(["Name", "Visa tag", "Status"])
+        table.setRowCount(len(self.equipment_list))
+        table.setColumnCount(len(self.equipment_list[0]))
+        table.setHorizontalHeaderLabels(["Name", "Visa tag", "Status",""])
 
-        for i, (name, tag, status) in enumerate(equipment_list):
+        for i, (name, tag, status, button) in enumerate(self.equipment_list):
             _name = QTableWidgetItem(name)
             _tag = QTableWidgetItem(tag)
             _status = QTableWidgetItem(status)
             table.setItem(i, 0, _name)
             table.setItem(i, 1, _tag)
             table.setItem(i, 2, _status)
+            table.setCellWidget(i, 3, button)
+            button.clicked.connect(lambda checked, x=_tag: self.connect_equipment.emit(x))
 
         table.resizeRowsToContents()
         table.resizeColumnsToContents()
         table.show()
 
+        self.connect_equipment.connect(self._test)
+
         self.resource_list_thread = AvailableResourceList(self)
         self.resource_list_thread.start()
+
+    @QtCore.pyqtSlot(object)
+    def _test(self, name) -> None:
+        print(name.text())
 
     def closeEvent(self, event):
         print("Closing application...")
@@ -102,10 +110,22 @@ class AvailableResourceList(QtCore.QThread):
             self.parent.mutex.lock()
             resource_list = self.parent.rm.list_resources()
             self.parent.mutex.unlock()
-            self.parent.listWidget.clear()
-            self.parent.listWidget.addItems(resource_list)
+            old_resource_list = []
+            for i in range(self.parent.listWidget.count()):
+                old_resource_list.append(self.parent.listWidget.item(i).text())
+            for i in resource_list:
+                if i not in old_resource_list:
+                    self.parent.listWidget.addItem(i)
+                else:
+                    pass
+            for i in range(len(old_resource_list)):
+                if old_resource_list[i] not in resource_list:
+                    old_item = self.parent.listWidget.takeItem(i)
+                    del old_item
+                else:
+                    pass
 
-            self.sleep(3)
+            self.sleep(2)
 
     def stop(self) -> None:
         self.running = False
